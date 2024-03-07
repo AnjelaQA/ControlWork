c4ffb3e1c6e3022a3e966d2c8a9ffb2b08f09656
Anjela <anjela1996qa@gmail.com>
2024-03-07 12:49:54 +0300

    Создали метод FormulatingArrayResult

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿
+void FormulatingArrayResult(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+     if(array1[i].Length <= 3)
+        {
+         array2[j] = array1[i];
+         j++;
+        }
+    }
+}
+
